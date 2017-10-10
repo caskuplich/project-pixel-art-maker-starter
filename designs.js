0dc8c5ed16bc344f8382daadee89aad3547e4fb1
Cassiano Kuplich <crkuplich@openmailbox.org>
2017-10-10 18:26:27 -0300

    Implement grid creation

--- designs.js
+++ designs.js
@@ -1,10 +1,32 @@
 // Select color input
+
 // Select size input
+const inputHeight = $('#input_height');
+const inputWidth = $('#input_width');
+
+// Select the grid container
+const canvas = $('#pixel_canvas');
 
 // When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function (evt) {
+  evt.preventDefault();
+  clearGrid();
+  makeGrid();
+});
+
+function clearGrid() {
+  canvas.empty();
+}
 
 function makeGrid() {
+  const height = inputHeight.val();
+  const width = inputWidth.val();
 
-// Your code goes here!
-
+  for (let i = 0; i < height; i++) {
+    let row = $('<tr></tr>');
+    for (let j = 0; j < width; j++) {
+      row.append('<td></td>');
+    }
+    canvas.append(row);
+  }
 }
