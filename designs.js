8b9d2fb519aae975a25289f18ad8f9b8ccd06d68
Cassiano Kuplich <crkuplich@openmailbox.org>
2017-10-10 19:48:28 -0300

    Implement pixel painting

--- designs.js
+++ designs.js
@@ -1,4 +1,5 @@
 // Select color input
+const colorPicker = $('#colorPicker');
 
 // Select size input
 const inputHeight = $('#input_height');
@@ -7,17 +8,25 @@ const inputWidth = $('#input_width');
 // Select the grid container
 const canvas = $('#pixel_canvas');
 
-// When size is submitted by the user, call makeGrid()
+// When size is submitted by the user, create the grid
 $('#sizePicker').submit(function (evt) {
   evt.preventDefault();
   clearGrid();
   makeGrid();
 });
 
+// When a grid cell is clicked, paint the cell with the selected color
+canvas.on('click', 'td', function (evt) {
+  const color = colorPicker.val();
+  $(this).css('background-color', color);
+});
+
+// Deletes all grid cells from the grid container
 function clearGrid() {
   canvas.empty();
 }
 
+// Creates the grid with the size submitted by the user
 function makeGrid() {
   const height = inputHeight.val();
   const width = inputWidth.val();
